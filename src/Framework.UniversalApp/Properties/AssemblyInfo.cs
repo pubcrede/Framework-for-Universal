590ae4ee719384dc70646e29fd4caf390782b708
Genesys Source <developers@genesyssource.com>
2017-03-12 08:11:57 -0700

    2017.03 Iteration
    Summary of work items resolved in this iteration are:
    • Activity table renamed to ActivityContext to avoid naming collisions.
    • ActivityContext and ExceptionLog code-first objects now both will
    self-create their tables.
    • Added tech notation (Mvc, Wpf, etc.) to VisualStudioMarketplace.com
    icons.
    • Simplified the GenesysFramework.com and GetFramework.com checkout
    • Genesys Source private repo moved to visualstudio.com VSTS.
    • Setup Guide:
    http://docs.getframework.com/library/Genesys-Framework-Quick-Guide.pdf
    • Release notes: http://bit.ly/2m8ZhfQ
    
    We thank you for taking a look at our framework of code reuse solutions.
    
    Other sites of interest...
    Genesys Source Framework: http://www.genesysframework.com/
    Setup Quick-Guide:
    http://docs.getframework.com/library/Genesys-Framework-Quick-Guide.pdf
    Reference Docs: http://docs.getframework.com/
    Genesys Source on GitHub: http://www.github.com/GenesysSource/
    Genesys Source in Visual Studio: http://bit.ly/2mHpFkf
    
    Preview Disclaimer
    All Genesys software, services and source code provided, including the
    Genesys Source Framework, are currently in limited Preview, as outlined
    in our preview disclaimer notice available on
    http://www.GenesysFramework.com.

--- src/Framework.UniversalApp/Properties/AssemblyInfo.cs
+++ src/Framework.UniversalApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Genesys Framework Quick-Start for Universal")]
-[assembly: AssemblyDescription("Genesys Framework Quick-Start powers your resuable framework for your .Net Core PCL/libraries, Universal x-platform mobile apps and all .Net Full libraries and apps.")]
+[assembly: AssemblyTitle("Genesys Source Quick-Start for Universal")]
+[assembly: AssemblyDescription("Genesys Source Quick-Start powers your resuable framework for your .Net Core PCL/libraries, Universal x-platform mobile apps and all .Net Full libraries and apps.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Genesys Source")]
-[assembly: AssemblyProduct("Genesys Framework Quick-Start for Universal")]
+[assembly: AssemblyProduct("Genesys Source Quick-Start for Universal")]
 [assembly: AssemblyCopyright("Copyright © 2017 Genesys Source")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.17.01")]
-[assembly: AssemblyFileVersion("3.17.01")]
+[assembly: AssemblyVersion("3.17.04")]
+[assembly: AssemblyFileVersion("3.17.04")]
 [assembly: ComVisible(false)]
